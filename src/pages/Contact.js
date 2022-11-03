a855da87e84cc2db1914516e78536ce6bf53facb
Mejor <amujobabatunde7@gmail.com>
2022-11-03 15:36:09 +0100

    Style
    Design the contact page according to the design

---
+++ src/pages/Contact.js
@@ -0,0 +1,51 @@
+import '../styles/contact.css'
+const Contact = () => {
+    return (
+        <div className="Contact">
+            <header className='contact-header'>
+                <h1>Contact Me</h1>
+                <p>Hi there, contact me to ask me about anything in you have in your mind</p>
+            </header>
+            <main>
+                <form className='form-container'>
+
+                    <section className='contact__name'>
+                        <label htmlFor="first_name">
+                            First name
+                            <input required type="text" id="first__name" placeholder="Enter your first name" />
+                        </label>
+                        <label htmlFor="last_name">
+                            Last name
+                            <input required type="text" id="last__name" placeholder="Enter your last name" />
+                        </label>
+
+                    </section>
+
+                    <label htmlFor="email">
+                        Email
+                        <input required type="email" id="email" placeholder="yourname@email.com" />
+                    </label>
+
+                    <label htmlFor="message">
+                        Message
+                        <textarea required name="message" id="message" cols="28" rows="17" placeholder="Send a message and I'll reply as soon as possible"></textarea>
+
+                    </label>
+
+                    <label className='terms' htmlFor="terms">
+                        <input required type="checkbox" id='terms' />
+                        You agree to providing your data to Amujo Babatunde who may contact you.
+                    </label>
+
+                    <button id='btn__submit'> Send Message</button>
+
+                </form>
+            </main>
+
+
+
+        </div>
+    );
+}
+
+export default Contact;
---
+++ src/pages/Home.js
@@ -0,0 +1,65 @@
+import github from '../assests/github.svg';
+import slack from '../assests/slack-logo.svg';
+import motto from '../assests/motto.svg';
+
+const Home = () => {
+    return (<>
+        <div className="Home">
+            <header>
+                <section className='profile_section'>
+                    <img id='profile__img' src="https://images.unsplash.com/photo-1474447976065-67d23accb1e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=385&q=80" alt='profile' />
+                    <h4>Amujo Babatunde</h4>
+                    <span id='slack'>Slack: Mejor</span>
+
+                </section>
+
+            </header>
+            <main>
+                <section className='link_section'>
+
+
+                    <a id='twitter' href='https://twitter.com/M_E_J_O_R' target={'_blank'} rel="noreferrer">Twitter</a>
+
+                    <a id='btn__zuri' href='https://training.zuri.team/' target={'_blank'} rel="noreferrer">Zuri Team</a>
+
+                    <a id='books' href='http://books.zuri.team' target={'_blank'} rel="noreferrer">Zuri Books</a>
+
+                    <a id='book__python' href='https://books.zuri.team/python-for-beginners?ref_id=mejor' target={'_blank'} rel="noreferrer">Python Books</a>
+
+                    <a id='pitch' href='https://background.zuri.team' target={'_blank'} rel="noreferrer">Background Checks for Coders</a>
+
+                    <a id='book__design' href='https://books.zuri.team/design-rules' target={'_blank'} rel="noreferrer">Design Books</a>
+
+                    <div className='social_media'>
+
+                        <img src={slack} alt="slack" />
+
+                        <a href="https://github.com/M-E-J-O-R">
+                            <img src={github} alt="github" />
+                        </a>
+
+
+                    </div>
+                </section>
+
+
+            </main>
+            <hr />
+
+            <footer>
+                <section className='zuri_footer'>
+
+
+                    <p>Zuri<b>.</b>Intership</p>
+                    <p>HNG Internship 9 Frontend Task</p>
+                    <img src={motto} alt="motto" />
+                </section>
+
+
+            </footer>
+
+        </div>
+    </>);
+}
+
+export default Home;
